79e138d5ac287d228a487a8b195a5a00cebb0c5b
udemy_flutter <mahemoud.nasef@gmail.com>
2025-09-15 19:55:29 +0300

    change

--- lib/bmi_result_screen.dart
+++ lib/bmi_result_screen.dart
@@ -1,49 +1,42 @@
 import 'package:flutter/material.dart';
 
 class BmiResultScreen extends StatelessWidget {
-
   late final int result;
   late final bool isMale;
   late final int age;
 
   BmiResultScreen({
-   required this.age,
+    required this.age,
     required this.isMale,
-    required this.result
-
-});
+    required this.result,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI RESULTs'),
-        centerTitle: true,
-      ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.keyboard_arrow_left),
+          ),
+          title: Text('BMI RESULTs'), centerTitle: true),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               'Gender : $isMale',
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
             ),
             Text(
               'Result : $result',
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
             ),
             Text(
               'Age : $age',
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
             ),
           ],
         ),
